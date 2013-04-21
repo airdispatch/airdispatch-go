07683766ea5b61c00d20c7d3646a35678b30e878
Hunter Leath <h@hunterleath.com>
2013-04-21 19:22:32 -0400

    changed some style information

--- client/static/js/application.js
+++ client/static/js/application.js
@@ -1,21 +1,25 @@
 Event.observe(window, 'load', function() {
 
-	var streamGraph = new Grafico.StreamGraph($('mailHistory'),
+	/*
+	var streamGraph = new Grafico.StackGraph($('mailHistory'),
 		{
-			normalMail: [20, 40, 10, 5, 4],
-			newsletters: [5, 10, 15, 20]
+			normalMail: [20, 40, 10, 5, 4, 30, 40],
+			newsletters: [5, 10, 15, 20, 6, 6, 2]
 		},
 		{
 			background_color: '#e0e0e0',
+			draw_axis: false,
+			grid: false,
 			draw_hovers: true,
-			stream_label_threshold: 20,
-			labels: true,
-			stream_line_smooting: 'simple',
 			datalabels: {
 				normalMail: "Normal Email",
 				newsletters: "Newsletters"
 			},
 		}
 	);
+	*/
+
+	var sparkBar = new Grafico.SparkBar($('mailHistory'), [30, 15, 50, 20, 50, 12, 45]);
+	var sparkBar = new Grafico.SparkLine($('responseTime'), [30, 15, 50, 20, 50, 12, 45]);
 
 });
