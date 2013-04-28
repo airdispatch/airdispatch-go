e1ec3bbd9b6ece26fdf148ccc3300074914113d8
Brent Baumgartner <tennisplayer2341@gmail.com>
2013-04-27 23:02:17 -0400

    Added filters and ordeBy to the list, created filters and services js files

---
+++ client/static/js/filters.js
@@ -0,0 +1,7 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: brentbaumgartner
+ * Date: 27/04/2013
+ * Time: 21:09
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ client/static/js/services.js
@@ -0,0 +1,7 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: brentbaumgartner
+ * Date: 27/04/2013
+ * Time: 22:08
+ * To change this template use File | Settings | File Templates.
+ */
