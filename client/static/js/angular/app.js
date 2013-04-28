465ce89fba2bb1b5fe57d39a92267718ca4c0ec6
Brent Baumgartner <tennisplayer2341@gmail.com>
2013-04-27 20:45:54 -0400

    Port to Foundation

---
+++ client/static/js/angular/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: brentbaumgartner
+ * Date: 25/04/2013
+ * Time: 13:40
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ client/static/js/angular/controllers.js
@@ -0,0 +1,7 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: brentbaumgartner
+ * Date: 26/04/2013
+ * Time: 18:11
+ * To change this template use File | Settings | File Templates.
+ */
