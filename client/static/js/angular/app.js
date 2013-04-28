23b34aa53c2eac5a5ab9f54e780f5412e6c3e516
Brent Baumgartner <tennisplayer2341@gmail.com>
2013-04-27 20:47:05 -0400

    commit

--- client/static/js/angular/app.js
+++ client/static/js/angular/app.js
@@ -1,7 +1,1 @@
-/**
- * Created with IntelliJ IDEA.
- * User: brentbaumgartner
- * Date: 25/04/2013
- * Time: 13:40
- * To change this template use File | Settings | File Templates.
- */
+angular.module('DispatchApp', ['DispatchApp.controllers']);
