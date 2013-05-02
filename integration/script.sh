c6cd14b2dea22de3546d5ece063c6a37f1ec43ce
Hunter Leath <h@hunterleath.com>
2013-05-02 15:45:47 -0400

    Added Helpful Messages to the Integration Script

--- integration/script.sh
+++ integration/script.sh
@@ -1,9 +1,12 @@
+echo "Moving the Files into the Correct Go Package..."
 mkdir $CI_HOME/src/airdispat.ch
 mv $CI_HOME/src/github.com/huntaub/airdispatch-protocol/* $CI_HOME/src/airdispat.ch/
 cd $CI_HOME/src/airdispat.ch/
+echo "Installing Dependencies..."
 go get code.google.com/p/goprotobuf/proto
 go get code.google.com/p/go.crypto/ripemd160
 # go get github.com/hoisie/web
+echo "Installing Airdispatch..."
 go install airdispat.ch/airdispatch
 go install airdispat.ch/common
-echo "Testing has Finished"
+echo "Testing has Finished!"
