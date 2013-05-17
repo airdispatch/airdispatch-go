a3ef5088e55f4673ced7f1cbf2d58d00e76def18
Hunter Leath <h@hunterleath.com>
2013-05-17 11:15:20 -0400

    Changed the Integration Script to Just install Everything (go-style)

--- integration/script.sh
+++ integration/script.sh
@@ -2,11 +2,12 @@ echo "Moving the Files into the Correct Go Package..."
 mkdir $CI_HOME/src/airdispat.ch
 mv $CI_HOME/src/github.com/huntaub/airdispatch-protocol/* $CI_HOME/src/airdispat.ch/
 cd $CI_HOME/src/airdispat.ch/
-echo "Installing Dependencies..."
-go get code.google.com/p/goprotobuf/proto
-go get code.google.com/p/go.crypto/ripemd160
+# echo "Installing Dependencies..."
+# go get code.google.com/p/goprotobuf/proto
+# go get code.google.com/p/go.crypto/ripemd160
 # go get github.com/hoisie/web
 echo "Installing Airdispatch..."
-go install airdispat.ch/airdispatch
-go install airdispat.ch/common
+# go install airdispat.ch/airdispatch
+# go install airdispat.ch/common
+go get ./...
 echo "Testing has Finished!"
