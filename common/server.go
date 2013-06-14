c5bb9ed2aa5098661ef81371007c06cf8d0bd530
Hunter Leath <h@hunterleath.com>
2013-06-14 11:49:19 -0400

    Changed the Way that Tracker Lookup Works

--- common/server.go
+++ common/server.go
@@ -2,42 +2,58 @@ package common;
 
 import (
 	"net"
-	"fmt"
 	"airdispat.ch/airdispatch"
 	"code.google.com/p/goprotobuf/proto"
 	"crypto/ecdsa"
+	"strings"
+	"errors"
 )
 
 // A function that will get the Location of an Address
-func LookupLocation(toAddr string, trackerList []string, key *ecdsa.PrivateKey) string {
-	// Loop Over Every Connected Tracker
-	for _, v := range(trackerList) {
-		address, _ := net.ResolveTCPAddr("tcp", v)
-
-		// Connect to Tracker
-		conn, err := net.DialTCP("tcp", nil, address)
-		if err != nil {
-			fmt.Println(err)
-			fmt.Println("Unable to connect to the tracking server.")
-			continue
-		}
-
-		// Send a Query to the Tracker
-		finalLocation, err := SendQuery(conn, toAddr, key)
-		// Return the Address if there were no errors
-		if err == nil {
-			return finalLocation
-		}
-
-		// Close the connection
-		conn.Close()
+func LookupLocation(toAddr string, trackerList []string, key *ecdsa.PrivateKey) (string, error) {
+	switch GetAddressType(toAddr) {
+		case AirdispatchAddressNormal:
+			// Loop Over Every Connected Tracker
+			for _, value := range(trackerList) {
+				location, err := SendQuery(value, toAddr, key)
+				if err == nil {
+					return location, nil
+				}
+			}
+		case AirdispatchAddressLegacy:
+			addressParts := strings.Split(toAddr, "@")
+			return SendQuery(addressParts[1], addressParts[0], key)
+		case AirdispatchAddressDirect:
+			addressParts := strings.Split(toAddr, "@@")
+			return addressParts[1], nil
+		default:
+			// If we cannot determine the address type, return nothing
+			return "", errors.New("Could Not Determine the Type of Airdispatch Address")
 	}
-	// If we have not returned a location yet, we cannot return anything
-	return ""
+
+	// If we could not lookup the address, return nothing
+	return "", errors.New("Could Not Locate Address In Provided Trackers")
+}
+
+func SendQuery(tracker string, addr string, key *ecdsa.PrivateKey) (string, error) {
+	address, _ := net.ResolveTCPAddr("tcp", tracker)
+
+	// Connect to Tracker
+	conn, err := net.DialTCP("tcp", nil, address)
+
+	if err != nil {
+		return "", err
+	}
+
+	// Close the connection
+	defer conn.Close()
+
+	// Send a Query to the Tracker
+	return SendQueryToConnection(conn, addr, key)
 }
 
 // A function that will send a query message over a connection
-func SendQuery(conn net.Conn, addr string, key *ecdsa.PrivateKey) (string, error) {
+func SendQueryToConnection(conn net.Conn, addr string, key *ecdsa.PrivateKey) (string, error) {
 	// Create a new Query Message
 	newQuery := &airdispatch.AddressRequest {
 		Address: &addr,
@@ -60,4 +76,21 @@ func SendQuery(conn net.Conn, addr string, key *ecdsa.PrivateKey) (string, error
 
 	// Return the Location
 	return *newQueryResponse.ServerLocation, nil
+}
+
+type AirdispatchAddressType int
+var AirdispatchAddressNormal AirdispatchAddressType = 1
+var AirdispatchAddressLegacy AirdispatchAddressType = 2
+var AirdispatchAddressDirect AirdispatchAddressType = 3
+
+func GetAddressType(addr string) AirdispatchAddressType {
+	switch strings.Count(addr, "@") {
+		case 0:
+			return AirdispatchAddressNormal
+		case 1:
+			return AirdispatchAddressLegacy
+		case 2:
+			return AirdispatchAddressDirect
+	}
+	return -1
 }
