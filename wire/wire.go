6c456235acf0aa0d897fb4fdcd9932d2d3004a86
Hunter Leath <h@hunterleath.com>
2014-02-18 01:23:15 -0500

    Added some Constants to the Wire Package, changed the name of Signature Message

--- wire/wire.go
+++ wire/wire.go
@@ -3,4 +3,20 @@
 // certain important constants for AirDispatch servers.
 package wire
 
+import (
+// "code.google.com/p/goprotobuf/proto"
+)
+
 var Prefix []byte = []byte("AD")
+
+// The constants represent the three-letter codes that denote each type of
+// Airdispatch message. The names of each constant should make the message
+// that they each represent self-apparent.
+const (
+	MessageDescriptionCode = "MDE"
+	TranferMessageCode     = "XFM"
+	TranferMessageListCode = "XFL"
+	MailCode               = "MAI"
+	DataCode               = "DAT"
+	ErrorCode              = "ERR"
+)
