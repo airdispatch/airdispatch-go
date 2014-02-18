1d9e639aeaae2e05c104149ec6e2c32910fda45a
Hunter Leath <h@hunterleath.com>
2014-02-18 01:21:42 -0500

    Move Old Errors into new Errors Package (to be revisited)

--- errors/errors.go
+++ errors/errors.go
@@ -17,3 +17,16 @@ const (
 	NoMessages       int = 2
 	NotAuthorized    int = 4
 )
+
+var ADSigningError = errors.New("ADSigningError: Message is not properly signed.")
+var ADUnmarshallingError = errors.New("ADUnmarshallingError: Message could not be unmarshalled.")
+var ADTimeoutError = errors.New("ADTimeoutError: Operation was not able to be completed in the timeout period")
+
+var ADUnexpectedMessageTypeError = errors.New("ADUnexpectedMessageTypeError: Received a different type of message than expected.")
+
+var ADTrackerVerificationError = errors.New("ADTrackerVerificationError: Could not verify the tracker is who you should be talking to.")
+var ADTrackerListQueryError = errors.New("ADTrackerListQueryError: The address queried for could not be located in the tracker list provided.")
+
+var ADDecryptionError = errors.New("ADDecryptionError: The payload of the Mail cannot be decrypted because the ADKey was not passed in correctly.")
+
+var ADIncorrectParameterError = errors.New("ADIncorrectParameterError: One of the parameters of this function is out of bounds.")
