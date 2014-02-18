b45350ef7605f26be41ff7836f065ad557c9eb56
Hunter Leath <h@hunterleath.com>
2014-02-18 01:27:32 -0500

    Moved FS-Go into Identity Package, Removed Last Common Package files

--- common/doc.go
+++
@@ -1,5 +0,0 @@
-// The Airdispatch common framework abstracts most of the low-level
-// work out of Airdispatch applications. By interacting with the
-// common framework, all applications ensure that they are using the
-// latest version of the protocol.
-package common
