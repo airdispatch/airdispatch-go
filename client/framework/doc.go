e9c0f1fb85d11a3f7902e5b07de646389aebb586
Hunter Leath <h@hunterleath.com>
2014-02-19 18:03:36 -0500

    Removed Client Framework

--- client/framework/doc.go
+++
@@ -1,7 +0,0 @@
-// The Airdispatch Client framework contains methods and 
-// constants that allow people to interact with Airdispatch
-// servers.
-//
-// This framework effectively uses an Airdispatch key
-// to abstract common messages away.
-package framework
