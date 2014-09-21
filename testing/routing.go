04b6fb39bb463dd56836720826f400c95b647b54
Hunter Leath <h@hunterleath.com>
2014-09-21 10:25:25 -0400

    Upgraded Testing to Router Redirects

--- testing/routing.go
+++ testing/routing.go
@@ -1,8 +1,10 @@
 package testing
 
 import (
-	"airdispat.ch/identity"
 	"errors"
+
+	"airdispat.ch/identity"
+	"airdispat.ch/routing"
 )
 
 // Define a blank router that we can use for testing purposes.
@@ -10,7 +12,7 @@ type StaticRouter struct {
 	Keys []*identity.Identity
 }
 
-func (t *StaticRouter) Lookup(addr string) (*identity.Address, error) {
+func (t *StaticRouter) Lookup(addr string, typ routing.LookupType) (*identity.Address, error) {
 	for _, x := range t.Keys {
 		if x.Address.String() == addr {
 			return x.Address, nil
@@ -19,10 +21,10 @@ func (t *StaticRouter) Lookup(addr string) (*identity.Address, error) {
 	return nil, errors.New("Unable to find address.")
 }
 
-func (t *StaticRouter) Register(*identity.Identity, string) error {
+func (t *StaticRouter) Register(*identity.Identity, string, map[string]routing.Redirect) error {
 	return nil
 }
 
-func (t *StaticRouter) LookupAlias(alias string) (*identity.Address, error) {
+func (t *StaticRouter) LookupAlias(alias string, typ routing.LookupType) (*identity.Address, error) {
 	return nil, nil
 }
